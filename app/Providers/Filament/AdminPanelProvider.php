bbd818637a33b0af8b74590a95b8125aaaffd444
Nik Farees <nikfarees2015@gmail.com>
2026-02-21 09:10:35 +0800

    remove search bar global

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->globalSearch(false)
             ->sidebarCollapsibleOnDesktop()
             ->profile()
             ->login()
